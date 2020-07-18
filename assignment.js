4899688c03d9110aa865bf5879f3d9dd501ec88c
tasnim-nova <tasnimtareq.cu@gmail.com>
2020-07-18 13:42:27 +0600

    Assignment Done

---
+++ assignment.js
@@ -0,0 +1,71 @@
+// Feet To Mile Converting Program
+
+function feetToMIle(feet){
+    if(feet<0){
+        return "Warnning! Give a Positive Input.";
+    }
+    else{
+        var mile = feet*0.00018939;
+        return mile;
+    }
+}
+var result= feetToMIle(-50) ;
+console.log(result);
+
+// Wood Calculating Program
+
+function woodCalculator(chair,table,bed){
+    if(chair<0 || table<0 || bed<0){
+        return "Bad Input.Give a Positive Value!"
+    }
+    else{ 
+    var totalWood = chair*1 + table*3 + bed*5;
+    return totalWood;
+    }
+}
+var woodNeeded = woodCalculator(3,6,4);
+console.log("Wood Calculation for chair,Table, Bed: ", woodNeeded);
+
+// Brick Calculating Program
+
+function brickCalculator(buildingHeight){
+    if(buildingHeight<1){
+        return "No Brick ,No Building!";
+    }
+    else if(buildingHeight<=10){
+        totalBrick = buildingHeight*15*1000;
+        return totalBrick;
+    }
+    else if(buildingHeight>10 || buildingHeight<=20){
+        totalBrick1 = (buildingHeight-10)*12*1000 + 10*15*1000;
+        return totalBrick1
+    }
+    else{
+        totalBrick2 = (buildingHeight-20)*10*1000 + 20*12*1000 + 10*15*1000;
+        return totalBrick2;
+    }
+}
+let brickCalc = brickCalculator(50);
+console.log("Total Brick Needed:",brickCalc);
+
+// Shortest Name Friend Finding Program
+
+ function tinyFriend(friends){
+     if(friends.length<=0){
+         return "Give Some NAME!";
+     }
+     else{
+     var min = friends[0];
+     for (i=0; i<friends.length; i++){
+        element = friends[i];
+        if(element<min){
+            element = min;
+        }
+     }
+     return min;
+    }
+ }
+
+ var friends=['nova','subah','mohaimin','tareq','ferdouse','soleiman'git push];
+ var shortestFriendsName = tinyFriend(friends);
+ console.log(shortestFriendsName);
